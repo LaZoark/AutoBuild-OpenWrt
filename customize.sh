ba09fded9a726e041bd90f10cd814a1024ec5b3d
Wei-Chih, Chen <emersonchen086@icloud.com>
2023-09-06 17:30:51 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,8 +10,7 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # sed -i 's/timezone=\'UTC\'/timezone=\'CST-8\'/g' openwrt/package/base-files/files/bin/config_generate 
-sed -i 's/timezone=\'UTC\'/zonename=Asia\\/Taipei/g' openwrt/package/base-files/files/bin/config_generate
-
+sed -i "s/timezone=\'UTC\'/zonename=\'Asia\\/Taipei\'/g" openwrt/package/base-files/files/bin/config_generate
 #2. Clear the login password
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
