891eb91b1a997998561e045536b90267fb0b28c4
Wei-Chih, Chen <emersonchen086@icloud.com>
2023-09-06 15:48:07 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,8 +8,14 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
+uci set luci.main.lang=zh_tw
+uci commit luci
+uci set system.@system[0].timezone=CST-8
+uci set system.@system[0].zonename=Asia/Taipei
+uci commit system
+
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
