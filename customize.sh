3f3269782b08e3bfc9d81134b32f2820d2043088
Wei-Chih, Chen <emersonchen086@icloud.com>
2023-09-06 17:03:59 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,15 +4,13 @@
 # Lisence: MIT
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
+# Modified by LaZoark
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-uci set luci.main.lang=zh_tw
-uci commit luci
-uci set system.@system[0].timezone=CST-8
-uci set system.@system[0].zonename=Asia/Taipei
-uci commit system
+# sed -i 's/timezone=\'UTC\'/timezone=\'CST-8\'/g' openwrt/package/base-files/files/bin/config_generate 
+sed -i 's/timezone=\'UTC\'/zonename=Asia\\/Taipei/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
